02945cdaf03b28acfcf5c9c3ec47c70081ec80b7
lucas s <lucassouza.zip@gmail.com>
2022-09-11 21:02:50 -0300

    finish

---
+++ frontend/.history/src/components/NotificationButton/index_20220911205541.tsx
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
+import './styles.css';
+
+type Props = {
+    saleId: number;
+}
+
+function handleClick(id :number) {
+    axios.get(`${BASE_URL}/sales/${id}/notification`).then(resp => {
+        toast.info("SMS enviado com sucesso.");
+    }).catch(err => {
+        toast.info("Erro ao enviar o SMS.");
+        console.error(err)
+    });
+}
+
+function NotificationButton( {saleId} : Props) {
+    return (
+        <>
+            <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
+                <img src={icon} alt="Notificar" />
+            </div>
+        </>
+    )
+}
+
+export default NotificationButton;
